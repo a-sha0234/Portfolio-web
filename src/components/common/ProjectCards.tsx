4b891fec80d8b183f1903ee3e666235222357317
a-sha0234 <asharma_31@hotmail.com>
2023-01-26 21:05:07 +0000

    style project page

--- src/components/common/ProjectCards.tsx
+++ src/components/common/ProjectCards.tsx
@@ -3,7 +3,7 @@ import { AiFillGithub } from "react-icons/ai";
 import { MdLiveTv } from "react-icons/md";
 // import { CgReadme } from "react-icons/cg";
 import TechTags from "./technologyTags";
-import { Parallax } from "react-scroll-parallax";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
 interface Props {
   data: any;
@@ -20,17 +20,22 @@ const ProjectsCards: React.FC<Props> = ({ data }) => {
     <main className="projectCard">
       <article className="projectCard__screenshot">
         <img src={data.screenshots} />
+        {/* <Parallax scaleX={[0, 1]}>
+          <div className="line">se</div>
+        </Parallax> */}
       </article>
 
       <article className="projectCard__description">
-        <h1>{data.ProjectName}</h1>
-        <p>{data.description} </p>
+        <div>
+          <h1>{data.ProjectName}</h1>
+          <p style={{ color: "grey" }}>{data.description} </p>
+        </div>
         <div className="github">
           <a href={data.Live} target="_blank">
-            <MdLiveTv color="white" size={50} />
+            <MdLiveTv color="white" size={40} />
           </a>
           <a href={data.Repo} target="_blank">
-            <AiFillGithub color="white" size={50} />
+            <AiFillGithub color="white" size={40} />
           </a>
         </div>
       </article>
@@ -46,6 +51,9 @@ const ProjectsCards: React.FC<Props> = ({ data }) => {
           </a>
         </div> */}
       </article>
+      <Parallax scaleX={[0, 1]} opacity={[0, 7]}>
+        <div className="line">se</div>
+      </Parallax>
     </main>
   );
 };
--- src/projects.ts
+++ src/projects.ts
@@ -1,4 +1,6 @@
 import cloudsweaper from "../src/screenshots/Picture1.png";
+import ecom from "../src/screenshots/ecom.png";
+import porfolioSite from "../src/screenshots/arunWeb.png";
 // import css from "../src/technologies/css.png";
 // import sass from "../src/technologies/sass.png";
 // import javascript from "../src/technologies/javascript.png";
@@ -26,41 +28,42 @@ let data = [
     languageImages: [projectBackgroundImg],
     screenshots: [cloudsweaper],
     description:
-      "A responsive weather based application. Features include: a  ",
+      "A responsive weather based application. Features include: live data pulled from Openweather API, a reactive graph utalizing ChartJS and a geolocation feature  ",
     readMeLink: "https://github.com/a-sha0234/CloudSweaper#readme",
     Live: "https://cloud-sweaper.vercel.app/",
     Repo: "https://github.com/a-sha0234/CloudSweaper",
   },
   {
-    ProjectName: "CloudSweaper",
+    ProjectName: "TickTock",
     languagesUsed: [
-      { language: "CSS3", color: "#093145" },
+      { language: "SASS/CSS", color: "#093145" },
       { language: "TypeScript", color: "#9A2617" },
-      { language: "NextJs", color: "#C2571A" },
-      { language: "HTML", color: "#FF3F80" },
-      { language: "HTML", color: "#FF3F80" },
-      { language: "HTML", color: "#FF3F80" },
-      { language: "HTML", color: "#FF3F80" },
+      { language: "React", color: "#C2571A" },
+      { language: "Redux", color: "#FF3F80" },
     ],
     languageImages: [projectBackgroundImg],
-    screenshots: [cloudsweaper],
+    screenshots: [ecom],
     description:
-      "A responsive weather based application. Features include: a  ",
+      "A responsive ecommerce platform. Features include: add to cart, sorting feature and mobile responsiveness   ",
     readMeLink: "https://github.com/a-sha0234/CloudSweaper#readme",
     Live: "https://cloud-sweaper.vercel.app/",
     Repo: "https://github.com/a-sha0234/CloudSweaper",
   },
-  // {
-  //   ProjectName: "CloudSweaper",
-  //   languagesUsed: ["CSS3, TypeScript, NextJs, HTML"],
-  //   languageImages: [projectBackgroundImg],
-  //   screenshots: [cloudsweaper],
-  //   description:
-  //     "A responsive weather based application. Features include: a  ",
-  //   readMeLink: "https://github.com/a-sha0234/CloudSweaper#readme",
-  //   Live: "https://cloud-sweaper.vercel.app/",
-  //   Repo: "https://github.com/a-sha0234/CloudSweaper",
-  // },
+  {
+    ProjectName: "Aruns Portfolio website",
+    languagesUsed: [
+      { language: "SASS/CSS", color: "#093145" },
+      { language: "TypeScript", color: "#9A2617" },
+      { language: "React", color: "#C2571A" },
+    ],
+    languageImages: [projectBackgroundImg],
+    screenshots: [porfolioSite],
+    description:
+      "A responsive portfolio website. This website included parrallex scrolling, mobile responsiveness   ",
+    readMeLink: "https://github.com/a-sha0234/CloudSweaper#readme",
+    Live: "https://cloud-sweaper.vercel.app/",
+    Repo: "https://github.com/a-sha0234/CloudSweaper",
+  },
   // {
   //   ProjectName: "CloudSweaper",
   //   languagesUsed: ["CSS3, TypeScript, NextJs, HTML"],
